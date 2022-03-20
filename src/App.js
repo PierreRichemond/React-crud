a5bffa77cbe9dc8eefb163be78a97c13faba2ca9
= <pierreambroise@hotmail.fr>
2022-03-20 22:09:24 +0100

    added form for the AddUser page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home.js';
 import AddUser from './components/AddUser.js';
 import EditUser from './components/EditUser.js';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 function App() {
   return (
-    <div className="App">
+    <div style={{maxWidth:'30rem', margin:'4rem auto'}}>
       <Router>
         <h1>Nav</h1>
         <Routes>
